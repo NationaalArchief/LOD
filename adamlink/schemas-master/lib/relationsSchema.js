78879728e2a2a8fc68f3f1a35e239de96b392156
faina007 <57965826+faina007@users.noreply.github.com>
2021-12-02 11:54:53 +0100

    Delete adamlink directory

--- adamlink/schemas-master/lib/relationsSchema.js
+++
@@ -1,7 +0,0 @@
-var baseSchema = require('../json-schemas/relations.schema.json')
-
-module.exports = function(config) {
-  // Load relations from Histograph configuration
-  baseSchema.properties.type.enum = config.schemas.relations;
-  return baseSchema;
-};
